b35a03015dcc74e39d43d05fc1a49da7b61e4c12
Bleno Claus <blenoclaus@gmail.com>
2019-10-10 17:35:01 -0300

    Created exception handler

--- user/src/main/java/br/com/registration/user/client/endpoint/service/ClientService.java
+++ user/src/main/java/br/com/registration/user/client/endpoint/service/ClientService.java
@@ -2,10 +2,13 @@ package br.com.registration.user.client.endpoint.service;
 
 import br.com.registration.core.model.Client;
 import br.com.registration.core.repository.ClientRepository;
+import br.com.registration.user.exceptions.ObjectNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -19,4 +22,16 @@ public class ClientService {
         return clientRepository.findAll(pageable);
     }
 
+    public Client find(Long id) {
+        log.info(String.format("searching client: %d", id));
+        return clientRepository
+                .findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException(String.format("Client not found: %d", id)));
+    }
+
+    public void delete(Long id) {
+        clientRepository
+                .findById(id)
+                .ifPresent(client -> clientRepository.deleteById(id));
+    }
 }
