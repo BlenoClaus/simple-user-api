3c89d21ac2127b775ccd01c60c1eb66e4b543751
Bleno Claus <blenoclaus@gmail.com>
2019-10-10 17:36:04 -0300

    new enpoints:   - findById - deleteById

--- user/src/main/java/br/com/registration/user/client/endpoint/controller/ClientController.java
+++ user/src/main/java/br/com/registration/user/client/endpoint/controller/ClientController.java
@@ -8,21 +8,31 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
-@RequestMapping("courses")
+@RequestMapping("clients")
 @Slf4j
 public class ClientController {
-
     @Autowired
     private ClientService clientService;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    ResponseEntity<Iterable<Client>> list(Pageable pageable) {
+    public ResponseEntity<Iterable<Client>> list(Pageable pageable) {
         return new ResponseEntity<>(clientService.list(pageable), HttpStatus.OK);
+    }
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Client> find(@PathVariable Long id) {
+        Client client = clientService.find(id);
+        return ResponseEntity.ok().body(client);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        clientService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
